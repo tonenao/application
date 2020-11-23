28acbfd56c6866e5baf1493b0991927aae7490a6
tonenao <tonenao@gmail.com>
2020-11-23 17:17:28 +0900

    first commit

---
+++ core/Application.php
@@ -0,0 +1,169 @@
+<?php
+
+abstract class Application
+{
+  protected $debug = false;
+  protected $request;
+  protected $response;
+  protected $session;
+  protected $db_manager;
+  protected $login_action = array();
+
+
+  public function __construct($debug = false)
+  {
+    $this->setDebugMode($debug);
+    $this->initialize();
+    $this->configure();
+  }
+
+
+  protected function setDebugMode($debug)
+  {
+    if($debug){
+      $this->debug = true;
+      ini_set('display_errors', 1);
+      error_reporting(-1);
+    } else {
+      $this->debug = false;
+      ini_set('display_errors', 0);
+    }
+  }
+
+
+  protected function initialize()
+  {
+    $this->request = new Request();
+    $this->response = new Response();
+    $this->session = new Session();
+    $this->db_manager = new DbManager();
+    $this->router = new Router($this->registerRouters());
+  }
+
+
+  protected function configure()
+  {
+  }
+
+
+  abstract public function getRootDir();
+
+  abstract protected function registerRoutes();
+
+
+  public function isDebugMode()
+  {
+    return $this->debug;
+  }
+
+
+  public function getRequest()
+  {
+    return $this->request;
+  }
+
+
+  public function getResponse()
+  {
+    return $this->response;
+  }
+
+
+  public function getSession()
+  {
+    return $this->session;
+  }
+
+
+  public function getDbManager()
+  {
+    return $this->db_manager;
+  }
+
+
+  public function getControllerDir()
+  {
+    return $this->getRootDir() .'/controllers';
+  }
+
+
+  public function getViewDir()
+  {
+    return $this->getRootDir() .'/views';
+  }
+
+
+  public function getModeDir()
+  {
+    return $this->getRootDir() .'/models';
+  }
+
+
+  public function getWebDir()
+  {
+    return $this->getRootDir() .'/web';
+  }
+
+
+  public function run()
+  {
+    try{
+      $params = $this->router->resolve($this->request->getPathInfo());
+      if($params === false){
+        // todo-A
+        throw new HttpNotFoundException('No route found for' .$this->request->getpathInfo());
+      }
+
+      $controller = $params['controller'];
+      $action = $params['action'];
+
+      $this->runAction($controller, $action, $params);
+
+      $this->response->send();
+
+    } catch (HttpNotFoundException $e){
+      $this->render404page($e);
+
+    } catch (UnauthorizedActionException $e){
+      list($cntroller, $action) = $this->login_action;
+      $this->runAction($controller, $action);
+      
+    }
+  }
+
+
+  public function runAction($controller_name, $action, $params = array())
+  {
+    $controller_class = ucfirst($controller_name) .'Controller';
+
+    $controller = $this->findController($controller_class);
+    if($controller === false){
+      // todo-B
+      throw new HttpNotFoundException($controller_class .'controller is not found.');
+    }
+
+    $content = $this->findController($controller_class);
+
+    $this->response->setContent($content);
+  }
+
+
+  protected function findController($controller_class)
+  {
+    if(!class_exists($controller_class)){
+      $controller_file = $this->getControllerDir() .'/' .$controller_class .'.php';
+      
+      if(!is_readable($controller_file)){
+        return false;
+      } else {
+        require_once $controller_file;
+
+        if(!class_exists($controller_class)){
+          return false;
+        }
+      }
+    }
+
+    return new $controller_class($this);
+  }
+}
---
+++ core/HttpNotFoundException.php
@@ -0,0 +1,3 @@
+<?php
+
+class HttpNotFoundExeption extends Exception {};
